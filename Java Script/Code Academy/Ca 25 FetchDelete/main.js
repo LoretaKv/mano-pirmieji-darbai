0656baf903f0fc323e506806856d5a719ecd664f
LoretaKv <lorecike1@yahoo.co.uk>
2022-10-26 13:36:22 +0300

    fetch delete

--- Java Script/Code Academy/Ca 25 FetchDelete/main.js
+++ Java Script/Code Academy/Ca 25 FetchDelete/main.js
@@ -1,4 +1,12 @@
-import { getProducts } from "./getProducts.js";
-// import { showProducts } from "./showProducts.js";
-await getProducts();
-// showProducts();
+const getProducts = async () => {
+  try {
+    const response = await fetch("https://golden-whispering-show.glitch.me");
+    if (response.ok) {
+      const productsList = await response.json();
+      console.log(productsList);
+      return productsList;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
