da2c6ad90b6b202f62163c977073206f183a894a
LoretaKv <lorecike1@yahoo.co.uk>
2022-11-28 20:25:53 +0200

    Practice update

--- NODE JS/PRAKTIKA/mongodb-practise/index.js
+++ NODE JS/PRAKTIKA/mongodb-practise/index.js
@@ -40,7 +40,7 @@ app.post("/order", async (_, res) => {
       .collection("orders")
       .insertOne({
         orderDate: date,
-        class: "Food",
+        type: "Food",
         item: "Tomato",
         quantity: 6,
         orderCompleted: dateWhenCompleted,
@@ -53,26 +53,78 @@ app.post("/order", async (_, res) => {
   }
 });
 
-// app.patch("/order", async (req, res) => {
-//   const { id } = req.params;
-//   const { class, item, quantity } = req.body;
+app.patch("/order/:id", async (req, res) => {
+  const { id } = req.params;
+  const { type, item, quantity } = req.body;
+  try {
+    const con = await client.connect();
+    const db = con.db("orders-project");
 
-//   // todo: test whether age, firstName, lastName are provided properly
-//   try {
-//     const con = await client.connect();
-//     const db = con.db("orders-project");
+    const order = await db
+      .collection("orders")
+      .findOneAndUpdate(
+        { _id: ObjectId(id) },
+        { $set: { type, item, quantity } }
+      );
 
-//     const user = await db.collection("orders").findOneAndUpdate(
-//       { _id: ObjectId(id) }
-//       { $set: { class, item, quantity } }
-//     );
+    await con.close();
 
-//     await con.close();
+    res.send(order).end();
+  } catch (error) {
+    return res.send({ error }).end();
+  }
+});
 
-//     res.send(user).end();
-//   } catch (error) {
-//     return res.send({ error }).end();
-//   }
-// });
+app.delete("/order/order/:id", async (req, res) => {
+  const { id } = req.params;
+  const { type, item, quantity } = req.body;
+  try {
+    const con = await client.connect();
+    const db = con.db("orders-project");
+
+    const order = await db
+      .collection("orders")
+      .deleteOne({ _id: ObjectId(id) });
+
+    await con.close();
+
+    res.send(order).end();
+  } catch (error) {
+    return res.send({ error }).end();
+  }
+});
+
+app.post("/orders", async (req, res) => {
+  const newOrders = req.body;
+  const areOrdersProvided = Array.isArray(newOrders) && newOrders?.length;
+
+  const isCorrectOrder = (newOrder) => {
+    const { type, item, quantity } = newOrder;
+    console.log(newOrder);
+  };
+
+  if (!areOrdersProvided) {
+    return res.status(400).send("Please provide an array of objects").end();
+  }
+
+  newOrders.forEach(isCorrectOrder);
+
+  // identiska kaip virs jos
+  // newOrders.forEach((newOrder) => isCorrectOrder(newOrder));
+
+  try {
+    const con = await client.connect();
+    const order = await con
+      .db("orders-project")
+      .collection("orders")
+      .insertMany(newOrders);
+
+    await con.close();
+
+    return res.send(order);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
 
 app.listen(PORT, () => console.log(`server is running on port:${PORT}`));
