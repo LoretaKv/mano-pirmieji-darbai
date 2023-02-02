4548094e6c1fbdd64b6c1eb6d79d9f4d3a52b438
LoretaKv <lorecike1@yahoo.co.uk>
2023-02-02 18:19:48 +0200

    home work correction

--- REACT/react-in-class-app/src/Components/UserCard.tsx
+++ REACT/react-in-class-app/src/Components/UserCard.tsx
@@ -2,28 +2,28 @@ import { useState } from "react";
 
 export const UserCard = ({ user }: any) => {
   const [isIdShown, setIsIdShown] = useState(false);
-  const [isUserCardShown, setIsUserCardShown] = useState(true);
 
   const visibilityHandler = () => {
-    if (isUserCardShown) {
-      setIsIdShown(true);
-      return setIsUserCardShown(false);
-    }
+    setIsIdShown((prevIsIdShown) => !prevIsIdShown);
+    // if (!isIdShown) {
+    //   setIsIdShown(true);
+    //   return;
+    // }
 
-    setIsIdShown(false);
-    setIsUserCardShown(true);
+    // setIsIdShown(false);
   };
 
   return (
     <div onClick={visibilityHandler} className="user-container">
-      {isUserCardShown && (
+      {isIdShown ? (
+        <p>{user.id}</p>
+      ) : (
         <div>
           <p>{user.login}</p>
           <p>{user.url}</p>
           <p>{user.type}</p>
         </div>
       )}
-      {isIdShown && <p>{user.id}</p>}
     </div>
   );
 };
